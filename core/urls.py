64eecf1a90b26f6750564b25bf35f6bbc393deca
Yasü <yasu@192.168.1.105>
2025-12-11 23:54:36 +0300

    media fix

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
